f598761d622b3ea7c72a0332be76884179307493
Vitor Dev <vittorbassdev@gmail.com>
2020-06-11 19:16:04 -0300

    Teste Configuração de apontamento CSS E JS

--- T1_PHPModerno/FSPHP/01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
+++ T1_PHPModerno/FSPHP/01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>01-06 FSPHP: Iniciando um Projeto</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>T1 FSPHP - PSR4</title>
+  <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    
+
+  <?php
+    /**
+    * Bloco PHP | Variável 'start' | Comando de Saída
+    */
+
+    $start = "Vamos Começar";
+      echo "Olá Mundo!, {$start}";
+    ?>
+
+    <p id="js"> 
+      Loading
+    </p>
+
+    <script src="assets/script.js"> 
+      
+    </script>
 </body>
 </html>
