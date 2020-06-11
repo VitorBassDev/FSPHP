3b2c4a6e7e07fb1a1de9d45e43c19930ea679532
Vitor Dev <vittorbassdev@gmail.com>
2020-06-11 16:58:30 -0300

    Temporada 01_EP02

---
+++ T1_PHPModerno/FSPHP/01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01-06 FSPHP: Iniciando um Projeto</title>
+</head>
+<body>
+    
+</body>
+</html>
